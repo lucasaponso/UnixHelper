d0133912110ae4dfb3a8b16b9e453fd29e72fdc6
Lucas Aponso <lucasaponso@outlook.com>
2023-01-28 14:24:52 +1100

    adding some stuff jan 28

---
+++ inc/ping.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+ping -c1 $1 1>/dev/null 2>/dev/null
+SUCCESS=$?
+
+if [ $SUCCESS -eq 0 ]
+then
+  echo "$1 has replied"
+  echo "Ping Was Successful" | festival --tts
+else
+  echo "$1 didn't reply"
+  echo "Ping Was Fail" | festival --tts
+fi
+
+
+
