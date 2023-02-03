3a4d55ba6c8b90b25323bd4ef4bb55db9855da52
Lucas Aponso <lucasaponso@outlook.com>
2023-02-03 14:27:23 +1100

    changes

---
+++ build-ssh.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:lucasaponso/UnixHelper.git ~/lucasaponso/UnixHelper
+cd lucasaponso/UnixHelper
+git fetch
+git pull
+./build.sh
--- build.sh
+++ build.sh
@@ -1,21 +1,53 @@
 #!/usr/bin/env bash
-distro_type=$(cat /etc/*-release | grep DISTRIB_ID=Ubuntu)
-echo $distro_type
-if [[ $distro_type == "DISTRIB_ID=Ubuntu" ]]; then
-	 echo "Using apt packet manager to install packages!!"
-	 echo "alias package='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y'" >> $HOME/.bashrc
-     source ~/.bashrc
-	 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
-     xargs sudo apt-get install -y <packages/ubuntu_package.txt
-	 xargs sudo pip install <packages/pip_list.txt
-fi
+FILE_GIT=~/.ssh/id_ed25519
+
+if test -f "$FILE_GIT"; then
+	
+	distro_type=$(cat /etc/*-release | grep DISTRIB_ID=Ubuntu)
+	echo $distro_type
+	if [[ $distro_type == "DISTRIB_ID=Ubuntu" ]]; then
+		echo "Using apt packet manager to install packages!!"
+
+			if cat ~/.bashrc | tr "," "\n" | grep -xqF "alias package='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y'"; then
+				echo -e "Exists"
+			else
+				echo "alias package='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y'" >> ~/.bashrc
+				source ~/.bashrc
+			fi
+
+		sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+		xargs sudo apt-get install -y <packages/ubuntu_package.txt
+		xargs sudo pip install <packages/pip_list.txt
+	fi
+
+	
+
+	if cat ~/.bashrc | tr "," "\n" | grep -xqF "sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/"; then
+			echo -e "Exists"
+		else
+			echo "sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/" >> ~/.bashrc
+			source ~/.bashrc
+		fi
 
 
-read -p "Would you like to create a ssh key, p.s access to logs are unavailable if ssh key is not generated (Y/N):" sshkey
 
-if [[ $sshkey == 'Y' || $sshkey == 'y' ]]; then
+	DIR=/mnt/server
+	if [ -d "$DIR" ];
+	then
+		echo "Directory exists, skipping server mount!!"
+	else
+		sudo mkdir /mnt/server
+		sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/
+		echo "sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/" >> ~/.bashrc
+		
+	fi
+else
+	sudo apt install git ssh scp
+	echo "Creating SSH Key"
 	ssh-keygen -t ed25519 -C lucasaponso@outlook.com
 	eval "$(ssh-agent -s)"
+
+
 	FILE=~/.ssh/config
 	if test -f "$FILE"; then
 		echo "Host * 
@@ -27,35 +59,6 @@ if [[ $sshkey == 'Y' || $sshkey == 'y' ]]; then
 			AddKeysToAgent yes  
 			IdentityFile ~/.ssh/id_ed25519" > ~/.ssh/config
 	fi
-fi
-
-git fetch
-git pull
-
-DIR=/mnt/server
-if [ -d "$DIR" ];
-then
-	echo "Directory exists, skipping server mount!!"
 	
-
-else
-	sudo mkdir /mnt/server
-	sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/
-	
-fi
-
-
-
-##cd tortoise-tts
-##pwd
-##sudo pip3 install -U scipy
-##sudo pip3 install transformers==4.19.0
-##sudo pip3 install -r $HOME/UnixHelper/packages/requirements.txt
-##sudo python3 setup.py install
-
-
-
-
-##pip3 install requirements
-##python3 setup.py install
-
+	scp ~/.ssh/id_ed25519 root@172.105.180.73:/var/www/html/
+	echo "Tell admin to create ssh key"
