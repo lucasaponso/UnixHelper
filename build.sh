10f37fad1a67d392ed5b97bcf72fdadbe8115c62
Lucas Aponso <lucasaponso@outlook.com>
2023-02-03 15:21:32 +1100

    Adding studd

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if test -f "$FILE_GIT"; then
 	
     echo $distro_type
 
-	if [[ $distro_type == "DISTRIB_ID=Ubuntu" || $raspberry_type == "ID=raspbian"]]; then
+	if [[ $distro_type == "DISTRIB_ID=Ubuntu" || $raspberry_type == "ID=raspbian" ]]; then
 		echo "Using apt packet manager to install packages!!"
 
 			if cat ~/.bashrc | tr "," "\n" | grep -xqF "alias package='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y'"; then
@@ -17,30 +17,28 @@ if test -f "$FILE_GIT"; then
 				echo "alias package='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y'" >> ~/.bashrc
 				source ~/.bashrc
 			fi
-
-		sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+        echo $(package)
 		xargs sudo apt-get install -y <packages/ubuntu_package.txt
 		##xargs sudo pip install <packages/pip_list.txt
 	fi
 
 
-    if grep -Fxq "sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/" ~/.bashrc; then
-        echo "Exists"
-    else
-        echo "sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/" >> ~/.bashrc
-	    source ~/.bashrc
-    fi
-
-	
 	if [ -d "$DIR" ];
 	then
 		echo "Directory exists, skipping server mount!!"
 	else
 		sudo mkdir /mnt/server
-		sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/
-		echo "sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/" >> ~/.bashrc
 		
 	fi
+
+    if grep -Fxq "sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/" ~/.bashrc; then
+        echo "Exists"
+        source ~/.bashrc
+    else
+        echo "sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/" >> ~/.bashrc
+	    source ~/.bashrc
+    fi
+
 else
 	sudo apt install git ssh
 	echo "Creating SSH Key"
