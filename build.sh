52176dd478901f2d8df25008e2289b0dfb645048
Lucas Aponso <lucasaponso@outlook.com>
2023-02-06 19:35:56 +1100

    adding comments & netowrking and processes

--- build.sh
+++ build.sh
@@ -1,32 +1,51 @@
 #!/usr/bin/env bash
 
+##################################################################################
+## function: rm_config()
+## Remove all configurations including aliases, mount points and keys
+## Steps of function:
+## 1. source .bashrc file & ask user delete configs
+## 2. if yes remove alias, mounting point and ssh key
+## 3. if no echo ok
+##################################################################################
 rm_config() {
+
+. ~/.bashrc
 echo -n "Would you like to delete all configs (Y/N)"
 read delete_config 
 
 if [[ $delete_config == "Y" ]]; then
-	##Remove alias
 	sed -i -e '/alias package=/d' ~/.bashrc
-	##Remove mounting directory & command to link to server
 	sudo rm -rf /mnt/server
 	sed -i -e '/sudo sshfs/d' ~/.bashrc
-	##Remove ssh key
 	rm -rf ~/.ssh/id*
 else
 	echo "Ok, keeping current config"
 fi
 }
 
+##################################################################################
+## function: current_config()
+## Prints current configuration.
+## Steps of function:
+## 1. list /mnt directory to check for mount point
+## 2. list ~/.ssh to check for ssh key
+## 3. print .bashrc to check for configs
+##################################################################################
+
 current_config() {
 	sudo ls -als /mnt
 	ls -als ~/.ssh
+	. ~/.bashrc
 	echo ".bashrc configs:"
 	cat ~/.bashrc | grep "alias package"
 	echo ".bashrc configs:"
 	cat ~/.bashrc | grep "sudo sshfs"
+	
 }
 
 appending_config() {
+	. ~/.bashrc
 	FILE=~/.ssh/config
 	if test -f "$FILE"; then
 		echo "Host * 
@@ -40,12 +59,31 @@ appending_config() {
 	fi
 }
 
+process_networking() {
+
+echo "Wireless Interface:"
+echo $(ip a | grep "wlp3s0")
+echo "Physical Interface:"
+echo $(ip a | grep "eth0")
+
+inc/ping.sh 142.250.70.174
+inc/ping.sh 172.105.180.73
+inc/ping.sh 192.168.1.1
+inc/ping.sh 192.168.0.1
+
+echo "View systemctl processes (Y/N:)"
+read answer
+if $answer == 'Y'; then
+	sudo systemctl list-units --type=service -all >> /mnt/server/processes.txt
+fi
+}
 
 
 
-
+process_networking
 current_config
 rm_config
+
 FILE_GIT=~/.ssh/id_ed25519.pub
 DIR=/mnt/server
 distro_type=$(cat /etc/*-release | grep DISTRIB_ID=Ubuntu)
@@ -62,7 +100,7 @@ if test -f "$FILE_GIT"; then
 				echo ""
 			else
 				echo "alias package='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y'" >> ~/.bashrc
-				source ~/.bashrc
+				. ~/.bashrc
 			fi
         sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
 		xargs sudo apt-get install -y <packages/ubuntu_package.txt
@@ -75,16 +113,20 @@ if test -f "$FILE_GIT"; then
 		echo "Directory exists, skipping server mount!!"
 	else
 		sudo mkdir /mnt/server
+		. ~/.bashrc
 		
 	fi
 
-    if grep -Fxq "sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/" ~/.bashrc; then
-        echo "Exists"
-        source ~/.bashrc
+    if grep -Fxq "sudo sshfs -o allow_other,IdentityFile=$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server" ~/.bashrc; then
+        . ~/.bashrc
+		echo "Exists"
+        
     else
-        echo "sudo sshfs -o allow_other,IdentityFile=/$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server/" >> ~/.bashrc
-	    source ~/.bashrc
+		. ~/.bashrc
+        echo "sudo sshfs -o allow_other,IdentityFile=$HOME/.ssh/id_ed25519 root@172.105.180.73:/var/www/html /mnt/server" >> ~/.bashrc
+	    
     fi
+	. ~/.bashrc
 
 else
 	sudo apt install git ssh
@@ -97,6 +139,43 @@ else
 fi
 current_config
 rm_config
+
+echo "Would you like to install GUI applications(Y/N)"
+read answer
+
+if [[ $answer == 'Y' ]];
+then
+	echo "Ok"
+	sudo apt update
+	sudo apt install software-properties-common apt-transport-https wget -y
+	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+	sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+	sudo apt install code
+	code --version
+
+	sudo wget -O- https://download.sublimetext.com/sublimehq-pub.gpg | gpg –dearmor | sudo tee /usr/share/keyrings/sublimehq.gpg
+	echo ‘deb [signed-by=/usr/share/keyrings/sublimehq.gpg] https://download.sublimetext.com/ apt/stable/’ | sudo tee /etc/apt/sources.list.d/sublime-text.list
+	sudo apt update
+	sudo apt install sublime-text
+
+	sudo apt install software-properties-common apt-transport-https wget
+	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+	sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
+	sudo apt update
+	sudo apt install microsoft-edge-stable
+
+	sudo apt update
+	sudo apt install -y build-essential
+	sudo bash VMware-Workstation-Full-16.2.4-20089737.x86_64.bundle
+	sudo vmware-modconfig --console --install-all
+
+	sudo apt update
+	sudo add-apt-repository ppa:micahflee/ppa
+	sudo apt update
+	sudo apt install torbrowser-launcher
+else
+	echo "Ok, not installing GUI options"
+fi
 
 
 
--- inc/ping.sh
+++ inc/ping.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
-ping_status() {
 ping -c1 $1 1>/dev/null 2>/dev/null
 SUCCESS=$?
 
 if [ $SUCCESS -eq 0 ]
 then
   echo "$1 has replied"
-  echo "Ping Was Successful" | festival --tts
+  ##echo "Ping Was Successful" | festival --tts
 else
   echo "$1 didn't reply"
-  echo "Ping Was Fail" | festival --tts
+  ##echo "Ping Was Fail" | festival --tts
 fi
-}
-ping_status
 
 
