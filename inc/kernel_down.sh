50b7d17c4869bdf4de4b79ac76dc85b248067a26
lucasaponso <lucasaponso@lucasaponso-MacBookAir>
2023-02-16 10:57:51 +1100

    developed auto kernel download, updated package list

---
+++ inc/kernel_down.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+##uname -r
+##sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -Y
+##sudo rpi_update
+##uname -r
+
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.0.7.tar.xz
+tar xvf linux-6.0.7.tar.xz
+sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
+cd linux-6.0.7
+cp -v /boot/config-$(uname -r) .config
+make menuconfig
+make
+sudo make modules_install
+sudo make install
+rm -rf linux*
+sudo update-initramfs -c -k 6.0.7
+sudo update-grub
+sudo reboot
